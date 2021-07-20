5c1df7f8fce8c10c3210b226c741800d2aeeb074
Nadiia <37639998+NadezhdaEuphoria@users.noreply.github.com>
2021-07-21 00:49:45 +0300

    Add files via upload

--- swipe(14.07)/build/static/js/mouse.js
+++ swipe(14.07)/build/static/js/mouse.js
@@ -9,9 +9,32 @@ new Swiper('.image-slider', {
         dynamicBullets: true,
         renderBullet: function (index, className) {
             return '<span class="' + className + '">' + (index + 1) + '</span>';
-        },
+        }
     },
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true
+    },
+    grabCursor: true,
+    slideToClickedSlide:true,
+    hashNavigation: {
+        watchState:true,
+    },
+    keyboard: {
+        enabled:true,
+        onlyInViewport:true,
+        pageUpDown:true,
+    },
+    centeredSlides:true,
+    // mousewheel: {
+    //   sensitivity:1,
+    //     eventsTarget: ".image-slider"
+    // },
 });
+
+
+
+
 
 
 new Swiper('.image-slider2', {
@@ -19,18 +42,28 @@ new Swiper('.image-slider2', {
         el: '.swiper-pagination3',
         type: 'fraction',
         renderFraction: function (currentClass, totalClass) {
-            return ' Фото <span class="' + currentClass + '"></span>' + ' из ' + '<span class="' + totalClass + '"></span>';
+            return ' страница <span class="' + currentClass + '"></span>' + ' из ' + '<span class="' + totalClass + '"></span>';
         },
         // type:'progressbar'
     },
+    slidesPerView:3,
+    scrollbar: {
+        el: '.swiper-scrollbar2',
+        draggable: true
+    },
+    grabCursor: true,
+    autoHeight: true,
+    slidesPerGroup: 3,
 });
 
-
+//
 // pagination: {
 //     el: '.swiper-pagination2',
 //     type: 'fraction',
 //     renderFraction: function (currentClass, totalClass) {
 //         return ' Фото <span class="' + currentClass + '"></span>' + ' из ' + '<span class="' + totalClass + '"></span>';
 //     },
+//
+//
 
 
